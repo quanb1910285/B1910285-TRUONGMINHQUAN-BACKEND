e92f8cc817b5963170c3043a87258c1ceb0f4e12
quanb1910285 <quanb1910285@student.ctu.edu.vn>
2023-02-09 14:49:34 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 const contactsRouter = require("./app/routes/contact.route");
@@ -13,5 +15,15 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contact", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Interal Server Error",
+    });
+});
 
 module.exports = app;
